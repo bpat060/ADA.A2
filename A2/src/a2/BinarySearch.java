3e428520cb3d32181ef17ce2e2f2c0f817ff8dc2
sheetalskumar <sheetal.s.kumar@outlook.com>
2022-09-18 13:05:15 +1200

    l

--- A2/src/a2/BinarySearch.java
+++ A2/src/a2/BinarySearch.java
@@ -13,25 +13,25 @@ class BinarySearch {
 
     // Returns index of x if it is present in arr[l..
     // r], else return -1
-    int binarySearch(int arr[], int l, int r, int x) {
+    int binarySearch(int mxn[][], int l, int r, int x) {
         if (r >= l) {
             int mid = l + (r - l) / 2;
 
             // If the element is present at the
             // middle itself
-            if (arr[mid] == x) {
+            if (mxn[mid][0] == x) {
                 return mid;
             }
 
             // If element is smaller than mid, then
             // it can only be present in left subarray
-            if (arr[mid] > x) {
-                return binarySearch(arr, l, mid - 1, x);
+            if (mxn[mid][0] > x) {
+                return binarySearch(mxn, l, mid - 1, x);
             }
 
             // Else the element can only be present
             // in right subarray
-            return binarySearch(arr, mid + 1, r, x);
+            return binarySearch(mxn, mid + 1, r, x);
         }
 
         // We reach here when element is not present
@@ -42,15 +42,18 @@ class BinarySearch {
     // Driver method to test above
     public static void main(String args[]) {
         BinarySearch ob = new BinarySearch();
-        int arr[] = {2, 3, 4, 10, 40};
-        int n = arr.length;
-        int x = 10;
-        int result = ob.binarySearch(arr, 0, n - 1, x);
+        int[][] mxn = {
+            { 20, 40, 100, 130, 150, 200 },
+            { 40, 140, 250, 320, 400, 450},
+            { 100, 250, 350, 420, 450, 500},
+        };
+        int n = mxn.length;
+        int x = 40;
+        int result = ob.binarySearch(mxn, 0, n - 1, x);
         if (result == -1) {
             System.out.println("Element not present");
         } else {
-            System.out.println("Element found at index "
-                    + result);
+            System.out.println("Element found at index " + result);
         }
     }
 }
